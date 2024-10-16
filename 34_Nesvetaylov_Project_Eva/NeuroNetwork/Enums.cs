188d3291fb80286f06f391f7488dc28ac595998b
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-10-17 00:30:47 +0300

    Add files via upload

---
+++ 34_Nesvetaylov_Project_Eva/NeuroNetwork/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace _34_Nesvetaylov_Project_Eva
+{
+    enum NeuronType
+    {
+        Hidden,
+        Output
+    }
+    enum NetworkMode
+    {
+        Recognition,
+        Testing,
+        Learning
+    }
+    enum MemoryMode
+    {
+        GET, // чтение
+        SET, // сохранение
+        INIT // инициализация
+    }
+}
